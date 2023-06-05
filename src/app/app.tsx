a80a58239678772cd956f7658f61a211770e92f1
Juri <juri.strumpflohner@gmail.com>
2023-06-05 17:42:40 +0200

    modularization

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,15 @@
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
+import { ModulesOrders } from '@myreactapp/modules/orders';
+import { ModulesProducts } from '@myreactapp/modules/products';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="myreactapp" />
+      <ModulesOrders />
+      <ModulesProducts />
     </div>
   );
 }
