7d4b25bcd7fa9e9c27553cdad95fad8e949a1206
Juri <juri.strumpflohner@gmail.com>
2023-06-05 17:46:24 +0200

    add storybook

---
+++ modules/orders/src/lib/modules-orders.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { ModulesOrders } from './modules-orders';
+
+const Story: Meta<typeof ModulesOrders> = {
+  component: ModulesOrders,
+  title: 'ModulesOrders',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
